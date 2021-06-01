54837705182fafc5479805243aa2d5d9d2e831c5
Christophe Espiau <christophe.espiau@gmail.com>
2021-06-01 10:36:00 +0200

    Mise à jour du module contrib scald_twitter : 7.x-1.0-rc5 -> 7.x-1.2

--- sites/all/modules/contrib/scald_twitter/scald_twitter.js
+++ sites/all/modules/contrib/scald_twitter/scald_twitter.js
@@ -1,16 +1,20 @@
-(function($) {
+/**
+ * @file
+ */
+
+(function ($) {
 
 // I'm not sure if Twitter embedded widget work with AJAX content, so I'm just
 // using domready.
-$(function() {
+$(function () {
   var notfound = true;
-  $('.node').each(function() {
+  $('.node').each(function () {
     if ($(this).find('blockquote.twitter-tweet').length > 0) {
       if ($(this).hasClass('node-full')) {
         notfound = false;
       }
       else {
-        // Use simple form for teaser
+        // Use simple form for teaser.
         $(this).find('blockquote.twitter-tweet').removeClass('twitter-tweet');
       }
     }
